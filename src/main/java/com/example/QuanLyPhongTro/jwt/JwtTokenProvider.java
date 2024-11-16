943378dbc861f9badf7625c1a3c920517c9c31c1
anhdeptrei <killer2892003@gmail.com>
2024-11-16 17:35:22 +0700

    Bổ sung Jwt

---
+++ src/main/java/com/example/QuanLyPhongTro/jwt/JwtTokenProvider.java
@@ -0,0 +1,79 @@
+package com.example.QuanLyPhongTro.jwt;
+
+import com.example.QuanLyPhongTro.models.Admins;
+import com.example.QuanLyPhongTro.models.CustomUserDetails;
+import com.example.QuanLyPhongTro.models.Users;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+import io.jsonwebtoken.*;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+@Component
+@Slf4j
+public class JwtTokenProvider {
+    // Đoạn JWT_SECRET này là bí mật, chỉ có phía server biết
+    private final SecretKey JWT_SECRET = Keys.secretKeyFor(SignatureAlgorithm.HS512);
+
+    //Thời gian có hiệu lực của chuỗi jwt
+    private final long JWT_EXPIRATION = 604800000L;
+
+    // Tạo ra jwt từ thông tin user
+    public String generateToken(CustomUserDetails userDetails) {
+        Date now = new Date();
+        Date expiryDate = new Date(now.getTime() + JWT_EXPIRATION);
+
+        int userId;
+        if (userDetails.getUser() instanceof Users) {
+            userId = ((Users) userDetails.getUser()).getId();
+        } else if (userDetails.getUser() instanceof Admins) {
+            userId = ((Admins) userDetails.getUser()).getId();
+        } else {
+            throw new IllegalArgumentException("User type is not supported");
+        }
+
+        return Jwts.builder()
+                .setSubject(Long.toString(userId))
+                .setIssuedAt(now)
+                .setExpiration(expiryDate)
+                .signWith(JWT_SECRET)
+                .compact();
+    }
+
+    // Lấy thông tin user từ jwt
+    public int getUserIdFromJWT(String token) {
+        Claims claims = Jwts.parser()
+                .setSigningKey(JWT_SECRET)
+                .parseClaimsJws(token)
+                .getBody();
+
+        return Integer.parseInt(claims.getSubject());
+    }
+
+    // Phương thức để lấy tên người dùng từ token
+    public String getUsernameFromToken(String token) {
+        Claims claims = Jwts.parser()
+                .setSigningKey(JWT_SECRET)
+                .parseClaimsJws(token)
+                .getBody();
+        return claims.getSubject();
+    }
+
+    public boolean validateToken(String authToken) {
+        try {
+            Jwts.parser().setSigningKey(JWT_SECRET).parseClaimsJws(authToken);
+            return true;
+        } catch (MalformedJwtException ex) {
+            log.error("Invalid JWT token");
+        } catch (ExpiredJwtException ex) {
+            log.error("Expired JWT token");
+        } catch (UnsupportedJwtException ex) {
+            log.error("Unsupported JWT token");
+        } catch (IllegalArgumentException ex) {
+            log.error("JWT claims string is empty.");
+        }
+        return false;
+    }
+}
---
+++ src/main/java/com/example/QuanLyPhongTro/services/CustomUserDetailsService.java
@@ -0,0 +1,54 @@
+package com.example.QuanLyPhongTro.services;
+
+import com.example.QuanLyPhongTro.models.Admins;
+import com.example.QuanLyPhongTro.models.CustomUserDetails;
+import com.example.QuanLyPhongTro.models.Users;
+import com.example.QuanLyPhongTro.repositories.AdminsRepository;
+import com.example.QuanLyPhongTro.repositories.UsersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UsersRepository userRepository;
+
+    @Autowired
+    private AdminsRepository adminRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) {
+        // Kiểm tra xem user có tồn tại trong database không?
+        Users user = userRepository.findByUsername(username);
+        if (user != null) {
+            return new CustomUserDetails(user);
+        }
+
+        Admins admin = adminRepository.findByUsername(username);
+        if (admin != null) {
+            return new CustomUserDetails(admin);
+        }
+
+        throw new UsernameNotFoundException(username);
+    }
+
+    public CustomUserDetails loadUserById(int userId) {
+        Users user = userRepository.findById(userId).orElse(null);
+        if (user != null) {
+            return new CustomUserDetails(user);
+        }
+
+        Admins admin = adminRepository.findById(userId).orElse(null); // Nếu bạn cần kiểm tra Admin
+        if (admin != null) {
+            return new CustomUserDetails(admin);
+        }
+
+        throw new UsernameNotFoundException("User not found with id: " + userId);
+    }
+}
